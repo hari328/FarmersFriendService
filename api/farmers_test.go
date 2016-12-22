6b05af79bf3b479b470fa77aa0475ca7e6cf6db1
hari328 <harish.hi12321@gmail.com>
2016-12-22 09:55:04 +0530

    refactored tests

--- api/farmers_test.go
+++ api/farmers_test.go
@@ -9,6 +9,23 @@ import (
 	"encoding/json"
 )
 
+
+func getFarmers() Farmers {
+	mockFarmerData := Farmers{List:make([]Farmer,0)}
+
+	mockFarmerData.List = append(mockFarmerData.List, Farmer{Id:1, Name:"harish", District:"belgam", State:"Karnataka", PhoneNumber:8989829802})
+	mockFarmerData.List = append(mockFarmerData.List, Farmer{Id:2, Name:"palli", District:"kundapur", State:"Karnataka", PhoneNumber:9099009900})
+	return mockFarmerData
+}
+
+func mockDbResponse(farmers Farmers) sqlmock.Rows{
+	rows := sqlmock.NewRows([]string{"id", "name", "district", "state", "phoneNumber"}).
+		AddRow(farmers.List[0].Id, farmers.List[0].Name, farmers.List[0].District, farmers.List[0].State, farmers.List[0].PhoneNumber).
+		AddRow(farmers.List[1].Id, farmers.List[1].Name, farmers.List[1].District, farmers.List[1].State, farmers.List[1].PhoneNumber)
+	return rows
+}
+
+
 func TestShouldGetFarmers(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
@@ -16,25 +33,18 @@ func TestShouldGetFarmers(t *testing.T) {
 	}
 	defer db.Close()
 
-	app := &Api{db}
+	mockData := getFarmers()
+	rows := mockDbResponse(mockData)
+	mock.ExpectQuery("^SELECT (.+) FROM farmers$").WillReturnRows(rows)
 
 	req, _ := http.NewRequest("GET", "http://localhost/farmers", nil)
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected while creating request", err)
 	}
-
-	mockFarmerData := Farmers{List:make([]Farmer,0)}
-	mockFarmerData.List = append(mockFarmerData.List, Farmer{Id:1, Name:"harish", District:"belgam", State:"Karnataka", PhoneNumber:8989829802})
-	mockFarmerData.List = append(mockFarmerData.List, Farmer{Id:2, Name:"palli", District:"kundapur", State:"Karnataka", PhoneNumber:9099009900})
-
 	w := httptest.NewRecorder()
-	rows := sqlmock.NewRows([]string{"id", "name", "district", "state", "phoneNumber"}).
-		AddRow(mockFarmerData.List[0].Id, mockFarmerData.List[0].Name, mockFarmerData.List[0].District, mockFarmerData.List[0].State, mockFarmerData.List[0].PhoneNumber).
-		AddRow(mockFarmerData.List[1].Id, mockFarmerData.List[1].Name, mockFarmerData.List[1].District, mockFarmerData.List[1].State, mockFarmerData.List[1].PhoneNumber)
-
-	mock.ExpectQuery("^SELECT (.+) FROM farmers$").WillReturnRows(rows)
 
 	// now we execute our request
+	app := &Api{db}
 	app.ListFarmers(w, req)
 
 	if w.Code != 200 {
@@ -43,6 +53,9 @@ func TestShouldGetFarmers(t *testing.T) {
 
 	farmers := Farmers{List:make([]Farmer,0)}
 	err = json.Unmarshal(w.Body.Bytes(), &farmers)
+	assert.Equal(t, mockData,farmers)
+}
 
-	assert.Equal(t,mockFarmerData,farmers)
+func TestShouldAddFarmer(t *testing.T) {
+
 }
